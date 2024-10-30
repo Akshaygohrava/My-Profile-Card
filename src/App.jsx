ca905bdf4ad246e0d59d0cab9d1dcc48776f88ac
AkshayGohrava <akshaygohrava@gmail.com>
2024-10-30 18:50:29 +0530

    My-Profile-Card

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,15 @@ function App() {
       <div className='container-profile flex flex-col gap-5 rounded-lg bg-stone-900'>
          <img className='rounded-full' src="Akshay-Gohrava.jpeg" alt="Akshay-Gohrava" srcset="" />
          <h1>Akshay-Gohrava</h1>
-        <h2 className=' text-lime-500 mb-3'>Mumbai, Maharastra</h2>
+        <h2 className=' text-lime-500 mb-1'>Mumbai, Maharastra</h2>
         <p className='text-slate-200'>"Front-End Devloper And Designer"</p>
         <button><a href="https://github.com/Akshaygohrava/">GitHub</a></button>
         <button><a href="https://in.linkedin.com/in/akshaygohrava">LinkeDin</a></button>
-        <button><a href="#">front-End Devloper</a></button>
+        <button><a href="">front-End Dev</a></button>
         <button><a href="">Twitter</a></button>
         <button><a href="">Instagram</a></button>
       </div>
     </>
   )
 }
-
 export default App
