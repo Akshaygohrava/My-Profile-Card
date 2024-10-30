a6c4087e06b4dec4360d160e497edc476d5ee2e5
AkshayGohrava <akshaygohrava@gmail.com>
2024-10-30 14:49:43 +0530

    My-Profile-Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './app.css'
-
 function App() {
 
   return (
@@ -9,13 +8,11 @@ function App() {
          <h1>Akshay-Gohrava</h1>
         <h2 className=' text-lime-500 mb-3'>Mumbai, Maharastra</h2>
         <p className='text-slate-200'>"Front-End Devloper And Designer"</p>
-
-        <button><a href="">GitHub</a></button>
-        <button><a href="">LinkeDin</a></button>
-        <button><a href="">front-End Devloper</a></button>
+        <button><a href="https://github.com/Akshaygohrava/">GitHub</a></button>
+        <button><a href="https://in.linkedin.com/in/akshaygohrava">LinkeDin</a></button>
+        <button><a href="#">front-End Devloper</a></button>
         <button><a href="">Twitter</a></button>
         <button><a href="">Instagram</a></button>
-
       </div>
     </>
   )
